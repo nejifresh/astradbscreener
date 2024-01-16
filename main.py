4a5bfc17ca9adf27a4ea7360d40ecb8672627841
Neji Tawo <nejifresh@gmail.com>
2024-01-15 17:21:58 -0700

    Ready to publish

---
+++ main.py
@@ -0,0 +1,63 @@
+import os
+from langchain.embeddings import OpenAIEmbeddings
+import streamlit as st
+import uuid
+from utils import *
+from dotenv import find_dotenv, load_dotenv
+from langchain.vectorstores import AstraDB
+
+
+def main():
+    load_dotenv(find_dotenv(), override=True)
+    st.set_page_config(page_title="Resume Screening Assistant")
+    st.title('HR - Resume Screening Assistant')
+    st.subheader('I can help you screen candidates')
+    load_dotenv(find_dotenv(), override=True)
+
+    init_astra_db()
+
+    job_description = st.text_area("Please past the 'JOB DESCRIPTION' here..", key=1)
+    document_count = st.text_input("No. of matching RESUMES to return ", key=2)
+
+    # upload the resumes
+    pdfs = st.file_uploader("Upload resumes here. Only PDF files are allowed", type=["pdf"], accept_multiple_files=True)
+    submit = st.button('Analyze resumes')
+
+    if submit:
+        with st.spinner('Uploading and analyzing documents'):
+
+            st.session_state["unique_id"] = uuid.uuid4().hex
+            # create the documents list from all the uploaded pdfs
+            docs = create_docs(pdfs, st.session_state["unique_id"])
+            # display the count of resumes that have been uploaded
+            st.write(len(docs))
+
+            # create emebeddings instance
+            embedding = OpenAIEmbeddings()
+            vstore = AstraDB(
+                namespace="neji",
+                embedding=embedding,
+                collection_name="resumes",
+                token=os.environ["ASTRA_DB_APPLICATION_TOKEN"],
+                api_endpoint=os.environ["ASTRA_DB_API_ENDPOINT"],
+            )
+
+            inserted_docs = vstore.add_documents(docs)
+            st.write(f"\nInserted {len(inserted_docs)} documents into vector store.")
+
+            #now perform similarity search to show candidates
+            relevant_docs = (vstore.similarity_search(job_description, k=document_count))
+
+            for item in range(len(relevant_docs)):
+                st.subheader("👉" + str(item + 1))
+                # display the filepath
+                st.write("**File** : " + relevant_docs[item].metadata["name"])
+                # add expander
+                with st.expander("Show me 👀"):
+                    summary = get_summary(relevant_docs[item])
+                    st.write(summary)
+            st.success('Hope I was able to save your time... Thanks')
+
+
+if __name__ == '__main__':
+    main()
---
+++ utils.py
@@ -0,0 +1,95 @@
+
+from langchain.chains.summarize import load_summarize_chain
+from astrapy.db import AstraDB
+import os
+from langchain.chat_models import ChatOpenAI
+from dotenv import find_dotenv, load_dotenv
+load_dotenv(find_dotenv(), override=True)
+
+
+def init_astra_db():
+    db = AstraDB(
+        namespace="neji",
+        token=os.environ["ASTRA_DB_APPLICATION_TOKEN"],
+        api_endpoint=os.environ["ASTRA_DB_API_ENDPOINT"])
+
+    print(f"Connected to Astra DB: {db.get_collections()}")
+
+
+def load_any_document(file):
+
+    name, extension = os.path.splitext(file)
+
+    if extension == ".pdf":
+        from langchain.document_loaders import PyPDFLoader
+
+        print(f"loading file {file}")
+        loader = PyPDFLoader(file)
+
+    elif extension == ".docx":
+        from langchain.document_loaders import Docx2txtLoader
+
+        print(f"loading file {file}")
+        loader = Docx2txtLoader(file)
+    elif extension == ".txt":
+        from langchain.document_loaders import TextLoader
+
+        print(f"loading file {file}")
+        loader = TextLoader(file)
+
+    else:
+        print("Non supported document format")
+        return None
+
+    data = loader.load()
+
+    return data
+
+
+def chunk_data(doc, chunk_size=256, chunk_overlap=10, metadata={}):
+    from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=chunk_size, chunk_overlap=chunk_overlap, separators=["\n", " \n"]
+    )
+
+    docs = text_splitter.split_documents(doc)
+    for doc in docs:
+        doc.metadata = metadata
+
+    return docs
+
+#ITERATE OVER FILES THAT USER UPLOADED
+def create_docs(user_pdf_list, unique_id):
+    output_docs = []
+    file_names = []
+    for document in user_pdf_list:
+        bytes_data = document.read()
+        file_names.append(document.name)
+
+        file_name = os.path.join("./uploads", document.name)
+        with open(file_name, "wb") as f:
+            f.write(bytes_data)
+        docs = load_any_document(file_name)
+
+        chunks = chunk_data(doc=docs,
+            chunk_size = 1000,
+            chunk_overlap = 200,
+            metadata={"name": document.name,
+                      "type": document.type,
+                      "size": document.size,
+                      "unique_id": unique_id},)
+        output_docs.extend(chunks)
+
+
+
+    return output_docs
+
+
+
+def get_summary(current_doc):
+    llm = ChatOpenAI()
+    # llm = HuggingFaceHub(repo_id='pszemraj/led-large-book-summary', model_kwargs={"temperature": 1e-10})
+    chain = load_summarize_chain(llm=llm, chain_type="map_reduce")
+    summary = chain.run([current_doc])
+    return summary
